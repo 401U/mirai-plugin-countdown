5a2f7736e3dc1964641f10a6e4fd8a21d1237e77
YehowahLiu <redme@live.cn>
2022-03-23 20:28:50 +0800

    添加自定义权限，阻止未授权用户跨通讯录控制此插件

--- src/main/kotlin/cc/redme/mirai/plugin/countdown/PluginMain.kt
+++ src/main/kotlin/cc/redme/mirai/plugin/countdown/PluginMain.kt
@@ -4,27 +4,13 @@ import cc.redme.mirai.plugin.countdown.command.CountdownCommand
 import cc.redme.mirai.plugin.countdown.data.PluginConfig
 import cc.redme.mirai.plugin.countdown.data.PluginData
 import net.mamoe.mirai.console.command.CommandManager
+import net.mamoe.mirai.console.permission.PermissionService
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.utils.info
-
-/**
- * 使用 kotlin 版请把
- * `src/main/resources/META-INF.services/net.mamoe.mirai.console.plugin.jvm.JvmPlugin`
- * 文件内容改成 `org.example.mirai.plugin.PluginMain` 也就是当前主类全类名
- *
- * 使用 kotlin 可以把 java 源集删除不会对项目有影响
- *
- * 在 `settings.gradle.kts` 里改构建的插件名称、依赖库和插件版本
- *
- * 在该示例下的 [JvmPluginDescription] 修改插件名称，id和版本，etc
- *
- * 可以使用 `src/test/kotlin/RunMirai.kt` 在 ide 里直接调试，
- * 不用复制到 mirai-console-loader 或其他启动器中调试
- */
 
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
@@ -38,19 +24,23 @@ object PluginMain : KotlinPlugin(
             一个简单的倒计时插件
         """.trimIndent()
         )
-        // author 和 info 可以删除.
     }
 ) {
+    val notifyPerm by lazy { PermissionService.INSTANCE.register(permissionId("notify"), "主动推送倒计时提醒的权限", parentPermission) }
+    val crossContactPerm by lazy {
+        PermissionService.INSTANCE.register(permissionId("cross-contact"), "跨联系人控制插件的权限", parentPermission)
+    }
     override fun onEnable() {
         logger.info { "Countdown Plugin loaded" }
         PluginConfig.reload()
         PluginData.reload()
         CommandManager.registerCommand(CountdownCommand)
+        notifyPerm
+        crossContactPerm
         val eventChannel = GlobalEventChannel.parentScope(this)
         eventChannel.subscribeAlways<GroupMessageEvent> {
             // 群消息
             CountdownTasker.checkKeyword(message, group)
-
         }
         eventChannel.subscribeAlways<FriendMessageEvent> {
             // 好友消息
--- src/main/kotlin/cc/redme/mirai/plugin/countdown/command/CountdownCommand.kt
+++ src/main/kotlin/cc/redme/mirai/plugin/countdown/command/CountdownCommand.kt
@@ -5,6 +5,7 @@ import cc.redme.mirai.plugin.countdown.PluginMain
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.descriptor.CommandArgumentParserException
+import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
 import net.mamoe.mirai.contact.Contact
 
 object CountdownCommand: CompositeCommand(
@@ -13,18 +14,30 @@ object CountdownCommand: CompositeCommand(
     description = "倒数日指令"
 ) {
     @SubCommand("add", "添加")
-    suspend fun CommandSender.add(name: String, time: String, pattern: String = "距离{name}还有{time}", contact: Contact=Contact())=sendMessage(
-        CountdownTasker.addCountdown(name, time, pattern, contact)
+    suspend fun CommandSender.add(name: String, time: String, pattern: String = "距离{name}还有{time}", contact: Contact?=null)=sendMessage(
+        when{
+            contact!=null && hasPermission(PluginMain.crossContactPerm)-> CountdownTasker.addCountdown(name, time, pattern, contact)
+            contact == null -> CountdownTasker.addCountdown(name, time, pattern, Contact())
+            else -> "权限不足"
+        }
     )
 
     @SubCommand("del", "delete", "remove", "删除")
-    suspend fun CommandSender.del(index: Int, contact: Contact=Contact())=sendMessage(
-        CountdownTasker.delCountdown(index, contact)
+    suspend fun CommandSender.del(index: Int, contact: Contact?=null)=sendMessage(
+        when{
+            contact != null && hasPermission(PluginMain.crossContactPerm) -> CountdownTasker.delCountdown(index, contact)
+            contact == null -> CountdownTasker.delCountdown(index, Contact())
+            else ->  "权限不足"
+        }
     )
 
     @SubCommand("list", "page", "列表")
-    suspend fun CommandSender.list(page: Int=1, contact: Contact=Contact())=sendMessage(
-        CountdownTasker.listCountdown(page, contact)
+    suspend fun CommandSender.list(page: Int=1, contact: Contact?=null)=sendMessage(
+        when{
+            contact != null && hasPermission(PluginMain.crossContactPerm) -> CountdownTasker.listCountdown(page, contact)
+            contact == null -> CountdownTasker.listCountdown(page, Contact())
+            else ->  "权限不足"
+        }
     )
 
     private fun CommandSender.Contact(): Contact = subject?:throw CommandArgumentParserException("无法从当前环境获取联系人")
